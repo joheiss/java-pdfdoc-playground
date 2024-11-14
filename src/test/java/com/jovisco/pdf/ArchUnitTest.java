0eab67f3c977e3a00b2322e439ff0f3a2f7d6e1d
Josef Heiss <josef.heiss@jovisco.de>
2024-11-14 14:28:55 +0100

    more ArchUnit tests

--- src/test/java/com/jovisco/pdf/ArchUnitTest.java
+++ src/test/java/com/jovisco/pdf/ArchUnitTest.java
@@ -9,6 +9,7 @@ import com.tngtech.archunit.library.GeneralCodingRules;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
 @AnalyzeClasses(packages = "com.jovisco.pdf", importOptions = ImportOption.DoNotIncludeTests.class)
 public class ArchUnitTest {
@@ -22,6 +23,10 @@ public class ArchUnitTest {
             .and(GeneralCodingRules.NO_CLASSES_SHOULD_USE_FIELD_INJECTION);
 
     @ArchTest
+    public static final ArchRule free_of_cycles =
+            slices().matching("com.jovisco.pdf.(*)..").should().beFreeOfCycles();
+
+    @ArchTest
     public static final ArchRule core_classes_not_depend_on_packages =
             noClasses().that().resideInAPackage("..core..")
                     .should().dependOnClassesThat().resideInAnyPackage("..base..", "..invoice..", "..shared..");
@@ -32,7 +37,22 @@ public class ArchUnitTest {
                     .should().dependOnClassesThat().resideInAnyPackage("..core..");
 
     @ArchTest
-    public static final ArchRule invoice_classes_should_depend_on_core_packages =
+    public static final ArchRule base_classes_should_only_have_dependents_in_base_invoice_and_shared_packages =
+            classes().that().resideInAPackage("..base..")
+                    .should().onlyHaveDependentClassesThat().resideInAnyPackage("..base..", "..invoice..", "..shared");
+
+    @ArchTest
+    public static final ArchRule invoice_classes_should_only_depend_on_core_packages =
             classes().that().resideInAPackage("..invoice..")
                     .should().dependOnClassesThat().resideInAnyPackage("..core..");
+
+    @ArchTest
+    public static final ArchRule invoice_classes_should_only_have_dependents_in_invoice_and_shared_packages =
+            classes().that().resideInAPackage("..invoice..")
+                    .should().onlyHaveDependentClassesThat().resideInAnyPackage("..invoice..", "..shared");
+
+    @ArchTest
+    public static final ArchRule shared_classes_should_only_have_dependents_in_shared_packages =
+            classes().that().resideInAPackage("..shared..")
+                    .should().onlyHaveDependentClassesThat().resideInAnyPackage("..shared");
 }
